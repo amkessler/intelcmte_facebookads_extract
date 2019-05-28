572848d7bea3a4e1b9f85b0a99fdd063c9f18985
amkessler <kessler.aaron@gmail.com>
2019-05-28 13:07:11 -0400

    get data from saved rds file instead of re-running entire script 02

--- 03_analysis.R
+++ 03_analysis.R
@@ -7,10 +7,16 @@ library(janitor)
 library(lubridate)
 
 
-# run function in step 02 to handle processing of extracting from the pdf file
-source("02_solve_for_all_function.R")
+#### run function in step 02 to handle processing of extracting from the pdf file
+# source("02_solve_for_all_function.R")
+
+
+# bring in the results data
+data <- readRDS("myresults_formatted.rds")
+
 
 # check if formatted dataset came in successfully
-myresults_formatted
+data
 
-glimpse(myresults_formatted)
+# see characteristics of the data columns
+glimpse(data)
